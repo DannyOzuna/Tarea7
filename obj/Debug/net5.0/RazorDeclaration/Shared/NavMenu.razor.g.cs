44698596740d12ecca9e1961892f856ac7d666c0
Danny Ozuna Vasquez <69773592+DannyOzuna@users.noreply.github.com>
2021-03-12 22:38:17 -0400

    Agregue el modulo de Configuracion

--- obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -97,7 +97,7 @@ using Tarea7.Helpers;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 23 "C:\Users\danny\Desktop\Tarea7\Shared\NavMenu.razor"
+#line 28 "C:\Users\danny\Desktop\Tarea7\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
